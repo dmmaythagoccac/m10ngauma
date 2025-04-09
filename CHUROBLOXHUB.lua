3d38cdee06286432e7c803fc1f40d227c3d4b896
dmmaythagoccac <mabinh245@gmail.com>
2025-04-09 17:27:46 +0700

    Add files via upload

--- CHUROBLOXHUB.lua
+++ CHUROBLOXHUB.lua
@@ -2133,7 +2133,7 @@ ImageButton.BorderSizePixel=0
 ImageButton.Position=UDim2.new(0.120833337-0.10, 0, 0.0952890813+0.01, 0)
 ImageButton.Size=UDim2.new(0, 50, 0, 50)
 ImageButton.Draggable=true
-ImageButton.Image="http://www.roblox.com/asset/?id= 133481892638825"
+ImageButton.Image= "rbxassetid://129934580298278"
 UICorner.Parent=ImageButton
 UICorner.CornerRadius=UDim.new(1, 10) 
 ParticleEmitter.Parent=ImageButton
