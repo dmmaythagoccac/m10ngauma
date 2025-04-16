3766fb49b0c36a6cfb1e26393a91c01c8477f3b7
dmmaythagoccac <mabinh245@gmail.com>
2025-04-16 14:29:31 +0700

    Add files via upload

---
+++ ui banana.lua
@@ -0,0 +1,10 @@
+local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
+local Windows = Fluent:CreateWindow({
+    Title="Chú Roblox Hub",
+    SubTitle="By karse", 
+    TabWidth=160, 
+    Theme="R2LX",
+    Acrylic=false,
+    Size=UDim2.fromOffset(500, 320), 
+    MinimizeKey=Enum.KeyCode.End
+})
